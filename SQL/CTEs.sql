da91cd9e70e3a45b05d6ba1703c0ffe54503440b
Mishywayu <michellewayua2@gmail.com>
2025-02-11 18:17:01 +0300

    Advanced MySQL Series ..CTEs

---
+++ SQL/CTEs.sql
@@ -0,0 +1,6 @@
+use parks_and_recreation;
+
+-- Common Table Expression - allow you to create temporary result sets that can be referenced within a 
+-- SELECT, INSERT, UPDATE, or DELETE statement. They make complex queries more readable and modular by
+--  breaking them into logical, manageable parts.
+
