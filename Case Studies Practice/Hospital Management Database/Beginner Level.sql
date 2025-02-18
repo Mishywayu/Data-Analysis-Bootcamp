39e2d4860974fedc3fe2ab5c61ee05a7bafc0f86
Mishywayu <michellewayua2@gmail.com>
2025-02-18 13:49:51 +0300

    Beginner Level MySQL Practice

--- Case Studies Practice/Hospital Management Database/Beginner Level.sql
+++ Case Studies Practice/Hospital Management Database/Beginner Level.sql
@@ -5,3 +5,20 @@ select * from bills;
 select * from doctors;
 select * from patients;
 select * from treatments;
+
+-- List all doctors and their specializations.
+select concat(first_name, ' ', last_name) as doctor_name,
+	   specialization
+from doctors;
+
+-- Retrieve all patients born after the year 2000.
+select concat(first_name, ' ',last_name) as patient_name,
+	   year(dob) as year
+from patients
+where year(dob) > 2000
+;
+
+-- Show all appointments scheduled for a specific date.
+select * from appointments
+where status = "Scheduled"
+;
